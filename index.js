fbc9efc89554d4961483e4cdf485d8e3afe76b88
Angga Pamungkas <anggaindriya@gmail.com>
2021-07-18 18:45:24 +0700

    integrate-rnn: installing react native navigation, setup eslint and flow

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 /**
  * @format
+ * @flow
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { Navigation } from 'react-native-navigation'
+import App from './App'
+import { name as appName } from './app.json'
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent(appName, () => App)
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      stack: {
+        children: [
+          {
+            component: {
+              name: appName,
+              options: {
+                topBar: {
+                  visible: false
+                }
+              }
+            }
+          }
+        ]
+      }
+    }
+  })
+})
